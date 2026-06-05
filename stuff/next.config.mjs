011aeefb579179e7a3a1071a6e568f21d8f985f1
oldhagnessballs-netizen <oldhagnessballs@gmail.com>
2026-06-05 08:00:36 -0400

    i did stuff
    hooray

---
+++ stuff/next.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  serverExternalPackages: ['@better-auth/kysely-adapter'],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      }
+    }
+    return config
+  },
+}
+
+export default nextConfig
